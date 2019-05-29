58dc2cd11d628ba3f2be3c251daab3354bcc159e
gch12345 <1274021602@qq.com>
2019-05-29 12:00:44 +0800

    修复bug

--- 19_5_28/19_5_28/test.c
+++ 19_5_28/19_5_28/test.c
@@ -30,7 +30,7 @@
 //每一行都按照从左到右递增的顺序排序，
 //每一列都按照从上到下递增的顺序排序。
 //请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
-int exist(int arr[][4], int row, int col, int a)
+int exist(int arr[4][4], int row, int col, int a)
 {
 	int i = 0;
 	while (1)
@@ -65,7 +65,7 @@ int main()
 	int row = sizeof(arr) / sizeof(arr[0]);
 	int col = sz / row;
 	int k = 0;
-	k = exist(arr[4][4], row, col, a);
+	k = exist(arr, row, col, a);
 	if (k)
 	{
 		printf("存在\n");
