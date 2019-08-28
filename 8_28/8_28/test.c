2fd20646ae369e2508eac7a14b6e689756f3707c
gch12345 <1274021602@qq.com>
2019-08-28 22:18:24 +0800

    提交代码

---
+++ 8_28/8_28/test.c
@@ -0,0 +1,79 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+int myAtoi(char * str){
+	assert(str != NULL);
+	int re = 0;
+	while (*str == ' ')
+	{
+		str++;
+	}
+	int sign = 0;
+	if (sign == 0 && *str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (sign == 0 && *str == '+')
+	{
+		sign = 1;
+		str++;
+	}
+	int i = 0;
+	while (*str >= '0'&&*str <= '9')
+	{
+		i++;
+		if (i > 10 && sign != -1)
+		{
+			return INT_MAX;
+		}
+		if (i > 10 && sign == -1)
+		{
+			return INT_MIN;
+		}
+		re = re * 10 + (*str - '0');
+		str++;
+	}
+
+	if (sign == -1)
+		re = re*sign;
+	return re;
+}
+
+int main()
+{
+	char *str = "-91283472332";
+	printf("%d\n", myAtoi(str));
+	system("pause");
+	return 0;
+}
+
+
+
+//给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+//假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。
+//请根据这个假设，如果反转后整数溢出那么就返回 0。
+#if 0
+int reverse(int x){
+	long long re = 0;
+	while (x != 0)
+	{
+		re = re * 10 + x % 10;
+		x /= 10;
+	}
+	if (re != (int)re)
+		return 0;
+	return re;
+}
+
+int main()
+{
+	int i = INT_MAX;
+	//int j = -11;
+	//printf("%d\n", j % 10);
+	printf("%d\n", reverse(i));
+	system("pause");
+	return 0;
+}
+#endif
