45537bd87adde41f598213985dae9b669c67917f
gch12345 <1274021602@qq.com>
2019-09-04 22:05:34 +0800

    提交代码

--- 9_04/9_04/test.c
+++ 9_04/9_04/test.c
@@ -3,7 +3,27 @@
 #include<stdlib.h>
 #include<assert.h>
 #include<stdbool.h>
-
+//将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+	if (l1 == NULL){
+		return l2;
+	}
+	else if (l2 == NULL){
+		return l1;
+	}
+	else if (l1->val < l2->val){
+		l1->next = mergeTwoLists(l1->next, l2);
+		return l1;
+	}
+	else{
+		l2->next = mergeTwoLists(l1, l2->next);
+		return l2;
+	}
+}
 
 
 
