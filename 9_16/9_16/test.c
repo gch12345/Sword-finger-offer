d8f7f618397debe4188ed760e7405254d3df8451
gch12345 <1274021602@qq.com>
2019-09-17 21:36:41 +0800

    提交代码

--- 9_16/9_16/test.c
+++ 9_16/9_16/test.c
@@ -1,6 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
+//编写一个程序，通过已填充的空格来解决数独问题。
+//一个数独的解法需遵循如下规则：
+//数字 1 - 9 在每一行只能出现一次。
+//数字 1 - 9 在每一列只能出现一次。
+//数字 1 - 9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+//
+//空白格用 '.' 表示。
 
 char board0[9][9] = { 0 };
 void isFill(int row[9][9], int col[9][9], int box[9][9], char **board, int fillNumb)
