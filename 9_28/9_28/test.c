7fd72f0d2dbe7127e72a17b160e986d7017fccf2
郭晨辉 <1274021602@qq.com>
2019-09-29 15:39:49 +0800

    全排列

--- 9_28/9_28/test.c
+++ 9_28/9_28/test.c
@@ -1,35 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 //给定一个没有重复数字的序列，返回其所有可能的全排列。
-void fun(int* nums, int numsSize, int**re, int* returnSize, int**returnColumnSizes, int* arr){
-	if (nums == NULL)
-	{
-		for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
-		{
-			re[*returnSize][i] = arr[i];
-		}
-		(*returnColumnSizes)[*returnSize] = sizeof(arr) / sizeof(arr[0]);
+void swap(int* nums, int a, int b) {
+	int temp = nums[a];
+	nums[a] = nums[b];
+	nums[b] = temp;
+}
+void  backtrack(int* nums, int numsSize, int** ret, int first, int* returnSize, int** returnColumnSizes) {
+	if (first == numsSize) {
+		returnColumnSizes[0][*returnSize] = numsSize;
+		ret[*returnSize] = (int*)calloc(numsSize, sizeof(int));
+		memcpy(ret[*returnSize], nums, numsSize*sizeof(int));
 		(*returnSize)++;
 		return;
 	}
-	for (int i = sizeof(arr) / sizeof(arr[0]) - numsSize; i < sizeof(arr) / sizeof(arr[0]); i++)
-	{
-		for (int j = 0; j < sizeof(arr) / sizeof(arr[0]); j++)
-		{
-			arr[i] = nums[j];
-			fun(nums + 1, numsSize - 1, re, returnSize, returnColumnSizes, arr);
-		}
+	int  i;
+	for (i = first; i<numsSize; i++) {
+		swap(nums, first, i);
+		backtrack(nums, numsSize, ret, first + 1, returnSize, returnColumnSizes);
+		swap(nums, first, i);
 	}
 }
-
-int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
-	int len = 1;
-	for (int i = 1; i <= numsSize; i++)
-		len *= i;
-	int** re = (int**)malloc(len * sizeof(int*));
-	for (int i = 0; i < len; i++)
-		re[i] = (int*)malloc(sizeof(int)* numsSize);
-	int* arr = (int*)malloc(sizeof(int)* numsSize);
-	fun(nums, numsSize, re, returnSize, returnColumnSizes, arr);
-	return re;
+int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+	int** ret = (int **)calloc(720, sizeof(int *));
+	returnColumnSizes[0] = (int*)calloc(720, sizeof(int));
+	*returnSize = 0;
+	backtrack(nums, numsSize, ret, 0, returnSize, returnColumnSizes);
+	return ret;
 }
