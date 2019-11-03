b92f86ae3919d25ab64d56e3acf6783a81027716
郭晨辉 <1274021602@qq.com>
2019-11-03 23:58:37 +0800

    Base

--- java/src/dataBase/Base.java
+++ java/src/dataBase/Base.java
@@ -8,12 +8,12 @@ public class Base {
     }
     static void template() throws SQLException {
         Connection conn = null;
-        Statement st = null;
+        PreparedStatement st = null;
         ResultSet rs = null;
         try {
             conn = JdbcUtils.getConnection();
-            st = conn.createStatement();
-            rs = st.executeQuery("select * from user;");
+            st = conn.prepareStatement("select * from user;");
+            rs = st.executeQuery();
             while (rs.next()) {
                 System.out.println(rs.getObject("id") + "\t" + rs.getObject("name") + "\t" +
                         rs.getObject("age"));
