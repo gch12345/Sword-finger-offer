09c74a0b6e21a206c10ed175437c6fad62945f1f
郭晨辉 <1274021602@qq.com>
2019-11-21 21:48:01 +0800

    MapAndSet.java

---
+++ java/src/MapAndSet/MapAndSet.java
@@ -0,0 +1,67 @@
+package MapAndSet;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+public class MapAndSet {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        for (Integer i : map.keySet()) {
+            Integer count = map.get(i);
+            if (count == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 复制带随机指针的链表
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        for (Node cur = head; cur != null; cur = cur.next) {
+            map.put(cur, new Node(cur.val, null, null));
+        }
+        for (Node cur = head; cur != null; cur = cur.next) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+        }
+        return map.get(head);
+    }
+    //宝石与石头
+    public int numJewelsInStones(String J, String S) {
+        int result = 0;
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < J.length(); i++) {
+            set.add(J.charAt(i));
+        }
+        for (int i = 0; i < S.length(); i++) {
+            if (set.contains(S.charAt(i))) {
+                result++;
+            }
+        }
+        return result;
+    }
+    //
+}
