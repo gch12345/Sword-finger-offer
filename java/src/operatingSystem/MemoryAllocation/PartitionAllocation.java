ab8820b9e5d95a7c16ab2b36466fad2ba5696fa0
郭晨辉 <1274021602@qq.com>
2019-12-29 22:51:28 +0800

    MemoryAllocation

---
+++ java/src/operatingSystem/MemoryAllocation/PartitionAllocation.java
@@ -0,0 +1,7 @@
+package operatingSystem.MemoryAllocation;
+
+import java.util.LinkedList;
+
+public interface PartitionAllocation {
+    void Algorithm(LinkedList linkedList);
+}
---
+++ java/src/operatingSystem/MemoryAllocation/jframe.java
@@ -0,0 +1,31 @@
+package operatingSystem.MemoryAllocation;
+import java.awt.*;
+
+import javax.swing.*;
+
+public class jframe extends JFrame{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public jframe(){
+        this.setTitle("test");
+        Container con = this.getContentPane();
+        this.setLayout(new GridLayout(10,5,5,5));        //设置7行3列垂直水平间隔为5
+        con.add(new JButton("进程名"));
+        con.add(new JButton("服务时间"));
+        con.add(new JButton("到达时间"));
+        con.add(new JButton("完成时间"));
+        con.add(new JButton("周转时间"));
+        for (int i = 0; i < 40; i++) {
+            con.add(new JButton());
+        }
+        this.setVisible(true);
+        this.setBounds(25, 25, 1500, 1000);
+    }
+    public static void main(String[] args) {
+        new jframe();
+    }
+}
