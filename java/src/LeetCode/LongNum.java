43af54d1ece483d044f99e26be03572a6a767329
郭晨辉 <1274021602@qq.com>
2020-02-12 22:16:58 +0800

    LeetCode

---
+++ java/src/LeetCode/LongNum.java
@@ -0,0 +1,66 @@
+package LeetCode;
+
+import java.util.*;
+
+public class LongNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        StringBuffer s = new StringBuffer();
+        while (sc.hasNext()) {
+            s.append(sc.next());
+        }
+        String string = s.toString();
+        int Index = -1;
+        int Len = 0;
+        for (int i = 0; i< string.length();i++) {
+            char c = string.charAt(i);
+            Boolean b = true;
+            if (c == '-') {
+                b = false;
+                i++;
+                c = string.charAt(i);
+            }
+            if (c >= '0' && c <= '9') {
+                int index = i;
+                int len = 0;
+                while (i < string.length() && string.charAt(i) >= '0' && string.charAt(i) <= '9') {
+                    len++;
+                    i++;
+                }
+                if (!b) {
+                    len++;
+                    index--;
+                }
+                if (len > Len) {
+                    Len = len;
+                    Index = index;
+                }
+            }
+        }
+        if (Len == 0) {
+            return;
+        } else {
+            for (int i = Index; i < Index + Len; i++) {
+                System.out.print(string.charAt(i));
+            }
+        }
+        LongNum l = new LongNum();
+        int[] arr = {4,5,1,6,2,7,3,8};
+        l.GetLeastNumbers_Solution(arr,4);
+    }
+
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int val : input) {
+            pq.add(val);
+            if (pq.size() > k)
+                pq.poll();
+        }
+        while (!pq.isEmpty()) {
+            int num = pq.poll();
+            arrayList.add(0, num);
+        }
+        return arrayList;
+    }
+}
