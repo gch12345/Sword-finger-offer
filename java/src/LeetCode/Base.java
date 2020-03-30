d251bc01afa80ce303985526712d725c472c9bd8
郭晨辉 <1274021602@qq.com>
2020-03-30 20:04:47 +0800

    LeetCode

---
+++ java/src/LeetCode/Base.java
@@ -0,0 +1,56 @@
+package LeetCode;
+public class Base
+{
+    private String baseName = "base";
+    public Base()
+    {
+        callName();
+    }
+
+    private void callName()
+    {
+        System. out. println(baseName);
+    }
+
+    static class Sub extends Base
+    {
+        private String baseName = "sub";
+        public void callName()
+        {
+            System. out. println (baseName) ;
+        }
+    }
+    public static void main(String[] args)
+    {
+        Base b = new Sub();
+        b.callName();
+    }
+}
+
+//public class Base
+//{
+//    private String baseName = "base";
+//    public Base()
+//    {
+//        callName();
+//    }
+//
+//    public void callName()
+//    {
+//        System. out. println(baseName);
+//    }
+//
+//    static class Sub extends Base
+//    {
+//        private String baseName = "sub";
+//        public void callName()
+//        {
+//            System. out. println (baseName) ;
+//        }
+//    }
+//    public static void main(String[] args)
+//    {
+//        Base b = new Sub();
+//        b.callName();
+//    }
+//}
