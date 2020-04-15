aed78559f43f6eddc4cd462f237c69649726c839
郭晨辉 <1274021602@qq.com>
2020-04-15 18:51:07 +0800

    LeetCode

--- java/src/LeetCode/MaxProfit.java
+++ java/src/LeetCode/MaxProfit.java
@@ -1,9 +1,5 @@
 package LeetCode;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-
 public class MaxProfit {
 
     //买卖股票的最佳时机
@@ -23,7 +19,7 @@ public class MaxProfit {
 
     public int maxProfit1(int[] prices) {
         int profit = 0;
-        for (int i = 0; i < prices.length;) {
+        for (int i = 0; i < prices.length; ) {
             int price = prices[i];
             while (i < prices.length - 1 && prices[i] < prices[i + 1]) {
                 i++;
@@ -33,19 +29,24 @@ public class MaxProfit {
         return profit;
     }
 
-    public int maxProfit(int[] prices) {
+    public int maxProfit2(int[] prices) {
         int max_k = 2;
         int[][][] dp = new int[prices.length][max_k + 1][2];
         for (int i = 0; i < prices.length; i++) {
-            for (int k = 0; k < 2; k++) {
+            for (int k = 1; k <= 2; k++) {
                 if (i == 0) {
                     dp[0][k][1] = -prices[0];
                     continue;
                 }
                 dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
-                dp[i][k][1] = Math.max(dp[i - 1][k][1], dp[i - 1][k + 1][0] - prices[i]);
+                dp[i][k][1] = Math.max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
+
+//                dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][1][1] + prices[i]);
+//                dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][0][0] - prices[i]);
+//                dp[i][2][0] = Math.max(dp[i - 1][2][0], dp[i - 1][2][1] + prices[i]);
+//                dp[i][2][1] = Math.max(dp[i - 1][2][1], dp[i - 1][1][0] - prices[1]);
             }
         }
-        return dp[prices.length - 1][0][0];
+        return dp[prices.length - 1][2][0];
     }
 }
