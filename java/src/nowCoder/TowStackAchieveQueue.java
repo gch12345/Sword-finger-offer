4460ac0558b6454694f948311141f71dcd5ada4e
郭晨辉 <1274021602@qq.com>
2020-04-26 22:16:53 +0800

    nowCoder

--- java/src/nowCoder/TowStackAchieveQueue.java
+++ java/src/nowCoder/TowStackAchieveQueue.java
@@ -1,21 +1,48 @@
 package nowCoder;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class TowStackAchieveQueue {
-    Stack<Integer> stack1 = new Stack<Integer>();
-    Stack<Integer> stack2 = new Stack<Integer>();
+    static Stack<Integer> stack1 = new Stack<Integer>();
+    static Stack<Integer> stack2 = new Stack<Integer>();
 
-    public void push(int node) {
+    public static void push(int node) {
         stack1.push(node);
     }
 
-    public int pop() {
+    public static int poll() {
         if (stack2.isEmpty()) {
             while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
             }
         }
         return stack2.pop();
+    }
+
+    public static int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int num = sc.nextInt();
+            sc.nextLine();
+            for (int i = 0; i < num; i++) {
+                String str = sc.nextLine();
+                if (str.equals("peek")) {
+                    System.out.println(peek());
+                } else if(str.equals("poll")) {
+                    poll();
+                } else {
+                    push(Integer.parseInt(str.substring(4)));
+                }
+            }
+        }
     }
 }
