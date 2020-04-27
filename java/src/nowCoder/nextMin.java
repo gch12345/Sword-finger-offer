f88179ca0fc4ee5d1649a28a4cdcbe32645136e3
郭晨辉 <1274021602@qq.com>
2020-04-27 20:55:42 +0800

    nowCoder

--- java/src/nowCoder/nextMin.java
+++ java/src/nowCoder/nextMin.java
@@ -1,38 +1,10 @@
 package nowCoder;
 
-import org.junit.Test;
-
 import java.util.Scanner;
 import java.util.Stack;
 
-public class findLeftRightLeaveMinIndex {
+public class nextMin {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            int len = sc.nextInt();
-            int[] arr = new int[len];
-            for (int i = 0; i < len; i++) {
-                arr[i] = sc.nextInt();
-            }
-            for (int i = 0; i < len; i++) {
-                int left = i;
-                int right = i;
-                while (left >= 0 && arr[left] >= arr[i]) {
-                    left--;
-                }
-                while (right < len && arr[right] >= arr[i]) {
-                    right++;
-                }
-                if (right >= len) {
-                    right = -1;
-                }
-                System.out.println(left + " " + right);
-            }
-        }
-    }
-
-    @Test
-    public void t0() {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             int num = sc.nextInt();
@@ -46,11 +18,11 @@ public class findLeftRightLeaveMinIndex {
                 while (true) {
                     if (stack.isEmpty()) {
                         left[i] = -1;
-                        stack.push(arr[i]);
+                        stack.push(i);
                         break;
                     } else if (arr[stack.peek()] < arr[i]) {
                         left[i] = stack.peek();
-                        stack.push(arr[i]);
+                        stack.push(i);
                         break;
                     } else {
                         stack.pop();
@@ -63,11 +35,11 @@ public class findLeftRightLeaveMinIndex {
                 while (true) {
                     if (stack.isEmpty()) {
                         right[i] = -1;
-                        stack.push(arr[i]);
+                        stack.push(i);
                         break;
                     } else if (arr[stack.peek()] < arr[i]) {
                         right[i] = stack.peek();
-                        stack.push(arr[i]);
+                        stack.push(i);
                         break;
                     } else {
                         stack.pop();
@@ -78,10 +50,5 @@ public class findLeftRightLeaveMinIndex {
                 System.out.println(left[i] + " " + right[i]);
             }
         }
-    }
-
-    @Test
-    public void t() {
-        System.out.println("FG");
     }
 }
