0c25cc86550a4af7e46a83a2a29b3f5b2d3afb8e
郭晨辉 <1274021602@qq.com>
2020-05-03 23:55:34 +0800

    LeetCode

--- java/src/LeetCode/Offer.java
+++ java/src/LeetCode/Offer.java
@@ -218,4 +218,40 @@ public class Offer {
             }
         }
     }
+
+    public int cuttingRope(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        if (n == 3) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for (int i = 4; i <= n; i++) {
+            for (int j = 1; j <= (i / 2); j++) {
+                dp[i] = Math.max(dp[i], dp[j] * dp[i - j]);
+            }
+        }
+        return dp[n];
+    }
+
+    public int cuttingRope(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 1; i <= (n + 1) / 2; i++) {
+            for (int j = i; j <= n; j++) {
+                dp[j] = Math.max(dp[j], dp[j - i] * i);
+            }
+        }
+        return dp[n];
+    }
 }
