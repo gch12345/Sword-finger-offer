e38948ce4602eefd86221a5f41d27f691a28356d
郭晨辉 <1274021602@qq.com>
2020-05-14 19:50:07 +0800

    LeetCode

--- java/src/LeetCode/dp.java
+++ java/src/LeetCode/dp.java
@@ -64,4 +64,161 @@ public class dp {
         }
         return retNum;
     }
+
+    public int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null) {
+            return 0;
+        }
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+        if (col == 0) {
+            return 0;
+        }
+        int[][] dp = new int[row][col];
+        for (int i = 0; i < col; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                break;
+            }
+            dp[0][i] = 1;
+        }
+        for (int i = 0; i < row; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            }
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+
+    public int minPathSum(int[][] grid) {
+        if (grid == null) {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] dp = new int[row][col];
+        int sum = 0;
+        for (int i = 0; i < col; i++) {
+            sum += grid[0][i];
+            dp[0][i] = sum;
+        }
+        sum = grid[0][0];
+        for (int i = 1; i < row; i++) {
+            sum += grid[i][0];
+            dp[i][0] = sum;
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+
+    public int backPackII(int m, int[] A, int[] V) {
+        if (m == 0 || A == null || V == null) {
+            return 0;
+        }
+        if (A.length == 0 || V.length == 0) {
+            return 0;
+        }
+        int len = A.length;
+        int[][] dp = new int[m + 1][len + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= len; j++) {
+                if (i < A[j - 1]) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - A[j - 1]][j - 1] + V[j - 1]);
+                }
+            }
+        }
+        return dp[m][len];
+    }
+
+    public int minCut(String s) {
+        if (s == null || s.length() <= 1) {
+            return 0;
+        }
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[0] = -1;
+        for (int i = 1; i <= len; i++) {
+            dp[i] = i - 1;
+        }
+        boolean[][] star = new boolean[len][len];
+        for (int i = 1; i <= len; i++) {//aa  2
+            for (int j = 0; j < i; j++) {
+                if (star[j][i - 1]) {//0 2
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[len];
+    }
+
+    private boolean[][] isPar(String s) {
+        if (s == null) {
+            return null;
+        }
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (i == j) {
+                    dp[j][i] = true;
+                } else if (i == j + 1) {
+                    dp[j][i] = (s.charAt(i) == s.charAt(j));
+                } else {
+                    dp[j][i] = dp[j + 1][i - 1] && (s.charAt(i) == s.charAt(j));
+                }
+            }
+        }
+        return dp;
+    }
+
+    private boolean isPar0(String substring) {
+        if (substring.length() == 0) {
+            return true;
+        }
+        int start = 0;
+        int end = substring.length() - 1;
+        char[] ch = substring.toCharArray();
+        while (start < end) {
+            if (ch[start] != ch[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
 }
