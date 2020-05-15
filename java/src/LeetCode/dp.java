9bf79583d9bca5de3003bdec37eb4df17388ace8
郭晨辉 <1274021602@qq.com>
2020-05-15 12:31:01 +0800

    LeetCode

--- java/src/LeetCode/dp.java
+++ java/src/LeetCode/dp.java
@@ -221,4 +221,62 @@ public class dp {
         }
         return true;
     }
+
+    public int minDistance(String word1, String word2) {
+        if (word1 == null || word2 == null) {
+            return -1;
+        }
+        int len1 = word1.length();
+        int len2 = word2.length();
+        if (len1 == 0) {
+            return len2;
+        }
+        if (len2 == 0) {
+            return len1;
+        }
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= len2; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                // 插入
+                dp[i][j] = dp[i][j - 1] + 1;
+                // 删除
+                dp[i][j] = Math.min(dp[i -  1][j] + 1, dp[i][j]);
+                // 替换
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
+                } else {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+
+    public int numDistinct(String S, String T) {
+        if (S == null || T == null) {
+            return -1;
+        }
+        int len1 = S.length();
+        int len2 = T.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= Math.min(i, len2); j++) {
+                if (S.charAt(i - 1) == T.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
 }
