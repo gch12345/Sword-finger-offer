ccf8132d8896e465993ec61ceaf7940cc0d1f212
郭晨辉 <1274021602@qq.com>
2020-05-20 14:16:48 +0800

    LeetCode

---
+++ java/src/Thread/Atomic.java
@@ -0,0 +1,9 @@
+package Thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Atomic {
+    private static AtomicInteger atomicInteger = new AtomicInteger();
+}
