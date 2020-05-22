5152860f7b870286f436318f307bd97cc19e2501
郭晨辉 <1274021602@qq.com>
2020-05-22 23:45:55 +0800

    LeetCode

--- java/src/LeetCode/dp.java
+++ java/src/LeetCode/dp.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Set;
 
 public class dp {
@@ -278,5 +279,44 @@ public class dp {
             }
         }
         return dp[len1][len2];
+    }
+
+    public static boolean checkSubarraySum(int[] nums, int k) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        int len = nums.length;
+        int[] arr = new int[len];
+        for (int i = 0; i < len - 1; i++) {
+            arr[i] = nums[i];
+            for (int j = i + 1; j < len; j++) {
+                arr[j] = arr[j - 1] + nums[j];
+                if (arr[j] == k || (k != 0 && arr[j] % k == 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int sum = 0;
+        HashMap < Integer, Integer > map = new HashMap< >();
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (k != 0)
+                sum = sum % k;
+            if (map.containsKey(sum)) {
+                if (i - map.get(sum) > 1)
+                    return true;
+            } else
+                map.put(sum, i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0};
+        checkSubarraySum(arr, 0);
     }
 }
