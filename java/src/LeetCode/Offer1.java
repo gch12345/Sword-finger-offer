2ef4da946b34a38d66df91da9edcf3e4698294e1
郭晨辉 <1274021602@qq.com>
2020-06-14 23:53:23 +0800

    LeetCode

--- java/src/LeetCode/Offer1.java
+++ java/src/LeetCode/Offer1.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.Stack;
+
 public class Offer1 {
     // 53 - II. 0～n-1中缺失的数字
     public int missingNumber(int[] nums) {
@@ -18,5 +20,53 @@ public class Offer1 {
             mid = left + (right - left) / 2;
         }
         return left;
+    }
+
+    //54. 二叉搜索树的第k大节点
+    // 中序遍历
+    public int kthLargest0(TreeNode root, int k) {
+        if (root == null || k <= 0) {
+            return Integer.MIN_VALUE;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        int count = 0;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.right;
+            }
+            cur = stack.pop();
+            count++;
+            if (count == k) {
+                break;
+            }
+            cur = cur.left;
+        }
+        return cur.val;
+    }
+
+    int k;
+    int ret;
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        if (root == null || k <= 0) {
+            return Integer.MAX_VALUE;
+        }
+        dfs(root);
+        return ret;
+    }
+
+    private void dfs(TreeNode root) {
+        if (k == 0 || root == null) {
+            return;
+        }
+        dfs(root.right);
+        if (k == 0) {
+            return;
+        }
+        ret = root.val;
+        k--;
+        dfs(root.left);
     }
 }
