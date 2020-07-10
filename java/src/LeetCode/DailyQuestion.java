7590f8f85733a7bc8c98be00df38aa1c5f91f044
郭晨辉 <1274021602@qq.com>
2020-07-10 12:00:42 +0800

    LeetCode

--- java/src/LeetCode/DailyQuestion.java
+++ java/src/LeetCode/DailyQuestion.java
@@ -134,6 +134,48 @@ public class DailyQuestion {
         return dp[sentence.length()];
     }
 
+    // 309. 最佳买卖股票时机含冷冻期
+    // dp[i][j][k]
+    // i -> 第几天
+    // j -> 是否有股票
+    // k -> 是否交易
+    public int maxProfit0(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int[][][] dp = new int[prices.length + 1][2][2];
+        dp[0][1][0] = Integer.MIN_VALUE;
+        for (int i = 1; i <= prices.length; i++) {
+            dp[i][0][1] = dp[i - 1][1][0] + prices[i - 1];
+            dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][0][0] - prices[i - 1]);
+            dp[i][0][0] = Math.max(dp[i - 1][0][0], dp[i - 1][0][1]);
+        }
+        return Math.max(dp[prices.length][0][0], dp[prices.length][0][1]);
+    }
+
+    // dp[i][j]
+    // i -> 第几天
+    // j -> 是否持有股票
+    // dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+    // dp[i][1] = max(dp[i - 1][1], dp[i - 2][0] - prides[i]);
+    // 当买入的时候 前天就不能有股票
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[prices.length + 1][2];
+        dp[0][1] = Integer.MIN_VALUE;
+        for (int i = 1; i <= prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+            if (i < 2) {
+                dp[i][1] = Math.max(dp[i - 1][1], -prices[i - 1]);
+            } else {
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i - 1]);
+            }
+        }
+        return dp[prices.length][0];
+    }
+
     public static void main(String[] args) {
         String[] strings = {"looked","just","like","her","brother"};
         DailyQuestion dailyQuestion = new DailyQuestion();
