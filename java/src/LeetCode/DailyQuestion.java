c7ff72e5fc8c599982dd059e27677cb989b1dad3
郭晨辉 <1274021602@qq.com>
2020-07-13 13:34:39 +0800

    LeetCode

--- java/src/LeetCode/DailyQuestion.java
+++ java/src/LeetCode/DailyQuestion.java
@@ -223,11 +223,64 @@ public class DailyQuestion {
         }
     }
 
+    // 350. 两个数组的交集 II
+    public int[] intersect0(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) {
+            return new int[0];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (map.containsKey(nums1[i])) {
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            } else {
+                map.put(nums1[i], 1);
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                list.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+        int[] ret = new int[list.size()];
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] ret = new int[list.size()];
+        for (i = 0; i < list.size(); i++) {
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
+
     public static void main(String[] args) {
         String[] strings = {"looked","just","like","her","brother"};
         DailyQuestion dailyQuestion = new DailyQuestion();
-        int[] nums = {4,2,6,1};
-        dailyQuestion.countSmaller(nums);
+        int[] nums = {1,2,2,1};
+        int[] nums1 = {2,2};
+        dailyQuestion.intersect(nums1, nums);
+//        dailyQuestion.countSmaller(nums);
 //        dailyQuestion.respace(strings, "jesslookedjustliketimherbrother");
     }
 }
