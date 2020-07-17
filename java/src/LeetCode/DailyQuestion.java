31b0008bddb1af3f13918b2a957051f746cd6dbe
郭晨辉 <1274021602@qq.com>
2020-07-17 14:55:14 +0800

    LeetCode

--- java/src/LeetCode/DailyQuestion.java
+++ java/src/LeetCode/DailyQuestion.java
@@ -395,18 +395,18 @@ public class DailyQuestion {
     }
 
     // 110. 平衡二叉树
-    public boolean isBalanced(TreeNode root) {
+    public boolean isBalanced0(TreeNode root) {
         return helper(root) != -1;
     }
-    private int helper(TreeNode root) {
+    private int helper0(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        int left = helper(root.left);
+        int left = helper0(root.left);
         if (left == -1) {
             return -1;
         }
-        int right = helper(root.right);
+        int right = helper0(root.right);
         if (right == -1) {
             return -1;
         }
@@ -526,6 +526,65 @@ public class DailyQuestion {
         }
         prev = root;
         helper(root.right);
+    }
+
+    //35. 搜索插入位置
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    // 剑指 Offer 11. 旋转数组的最小数字
+    public int minArray(int[] numbers) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (numbers[right] < numbers[mid]) {
+                left = mid + 1;
+            } else if (numbers[right] > numbers[mid]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+        return numbers[left];
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+        return helper(root) != -1;
+    }
+
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = helper(root.left);
+        if (left == -1) {
+            return -1;
+        }
+        int right = helper(root.right);
+        if (right == -1) {
+            return -1;
+        }
+        return Math.abs(left - right) <= 1 ? 1 + Math.max(left, right) : -1;
     }
 
     public static void main(String[] args) {
