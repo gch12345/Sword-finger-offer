8a81a17b64ba488c0c3945d3ba384c7e11e27533
郭晨辉 <1274021602@qq.com>
2020-10-04 23:38:15 +0800

    LeetCode

--- java/src/LeetCode/Offer2.java
+++ java/src/LeetCode/Offer2.java
@@ -462,6 +462,29 @@ public class Offer2 {
         return -1;
     }
 
+    // 47. 礼物的最大价值
+    public int maxValue(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return -1;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] dp = new int[row][col];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < col; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+    
     public static void main(String[] args) {
 //        TreeNode head = new TreeNode(0);
 //        head.left = new TreeNode(1);
