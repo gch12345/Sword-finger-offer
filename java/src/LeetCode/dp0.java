cf6bfa8efcac080b9f073e3a215f52a58abb7672
郭晨辉 <1274021602@qq.com>
2020-10-07 23:07:13 +0800

    LeetCode

--- java/src/LeetCode/dp0.java
+++ java/src/LeetCode/dp0.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Set;
 
 public class dp0 {
@@ -135,5 +136,61 @@ public class dp0 {
             }
         }
         return dp[len1][len2];
+    }
+
+    public int numDistinct(String S, String T) {
+        if (S == null || T == null) {
+            return -1;
+        }
+        int len1 = S.length();
+        int len2 = T.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= Math.min(i, len2); j++) {
+                if (S.charAt(i - 1) == T.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+
+    public static boolean checkSubarraySum(int[] nums, int k) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        int len = nums.length;
+        int[] arr = new int[len];
+        for (int i = 0; i < len - 1; i++) {
+            arr[i] = nums[i];
+            for (int j = i + 1; j < len; j++) {
+                arr[j] = arr[j - 1] + nums[j];
+                if (arr[j] == k || (k != 0 && arr[j] % k == 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean checkSubarraySum0(int[] nums, int k) {
+        int sum = 0;
+        HashMap< Integer, Integer > map = new HashMap< >();
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (k != 0)
+                sum = sum % k;
+            if (map.containsKey(sum)) {
+                if (i - map.get(sum) > 1)
+                    return true;
+            } else
+                map.put(sum, i);
+        }
+        return false;
     }
 }
