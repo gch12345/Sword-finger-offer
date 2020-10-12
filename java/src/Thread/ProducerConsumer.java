6604342ffa969ec78cd5708786348ef345fd90d8
郭晨辉 <1274021602@qq.com>
2020-10-12 23:27:44 +0800

    生产者消费者

--- java/src/Thread/ProducerConsumer.java
+++ java/src/Thread/ProducerConsumer.java
@@ -5,31 +5,31 @@ public class ProducerConsumer {
     public static volatile int size = 0;
     static class producer extends Thread{
         public producer(String name) {
-            Thread.currentThread().setName(name);
+            this.setName(name);
         }
 
         @Override
         public void run() {
             while (true) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
                 synchronized (ProducerConsumer.class) {
                     if (size < maxSize) {
                         size++;
                         System.out.println(Thread.currentThread().getName() + "生产者  size = " + size);
                         ProducerConsumer.class.notifyAll();
-
                     }
                     while (size == maxSize) {
+                        System.out.println("已经满了");
                         try {
                             ProducerConsumer.class.wait();
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }
@@ -37,17 +37,11 @@ public class ProducerConsumer {
 
     static class consumer extends Thread {
         public consumer(String name) {
-            Thread.currentThread().setName(name);
+            this.setName(name);
         }
-
         @Override
         public void run() {
             while (true) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
                 synchronized (ProducerConsumer.class) {
                     if (size > 0) {
                         size--;
@@ -55,6 +49,7 @@ public class ProducerConsumer {
                         ProducerConsumer.class.notifyAll();
                     }
                     while (size == 0) {
+                        System.out.println("没有可以消费的");
                         try {
                             ProducerConsumer.class.wait();
                         } catch (InterruptedException e) {
@@ -62,18 +57,23 @@ public class ProducerConsumer {
                         }
                     }
                 }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
     public static void main(String[] args) {
-        new consumer("c1").start();
-        new consumer("c2").start();
-        new consumer("c3").start();
-        new consumer("c3").start();
         new producer("p1").start();
         new producer("p2").start();
         new producer("p3").start();
         new producer("p4").start();
+//        new consumer("c1").start();
+        new consumer("c2").start();
+        new consumer("c3").start();
+        new consumer("c3").start();
     }
 }
