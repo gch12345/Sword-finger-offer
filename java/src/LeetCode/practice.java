0500775f8b0bb17148ec345ded538ff34c8d77ba
郭晨辉 <1274021602@qq.com>
2020-10-23 22:17:59 +0800

    LeetCode

--- java/src/LeetCode/practice.java
+++ java/src/LeetCode/practice.java
@@ -1,7 +1,6 @@
 package LeetCode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class practice {
     // 最长不重复子串
@@ -25,5 +24,64 @@ public class practice {
             }
         }
         return max;
+    }
+
+    // 二叉搜索树转双向链表
+    class Node{
+        int val;
+        Node left;
+        Node right;
+    }
+    public Node treeToDoublyList(Node root) {
+        if (root == null) {
+            return null;
+        }
+        helper(root);
+        head.left = tail;
+        tail.right = head;
+        return head;
+    }
+    Node head;
+    Node tail;
+    private void helper(Node root) {
+        if (root == null) {
+            return;
+        }
+        helper(root.left);
+        if (head == null) {
+            head = root;
+            tail = root;
+        } else {
+            tail.right = root;
+            root.left = tail;
+            tail = tail.right;
+        }
+        helper(root.right);
+    }
+
+    // 后序遍历
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode prev = null;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.add(cur);
+                cur = cur.left;
+            }
+            cur = stack.peek();
+            if (cur.right == prev || cur.right == null) {
+                list.add(cur.val);
+                prev = stack.pop();
+                cur = null;
+            } else {
+                cur = cur.right;
+            }
+        }
+        return list;
     }
 }
