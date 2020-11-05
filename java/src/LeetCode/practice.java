0e37bb3868bb34a1c0a27fddf81653e629c60e11
gch12345 <1274021602@qq.com>
2020-11-05 23:40:48 +0800

    压缩算法

--- java/src/LeetCode/practice.java
+++ java/src/LeetCode/practice.java
@@ -191,6 +191,40 @@ public class practice {
         return stringBuffer.reverse().toString();
     }
 
+    /**
+     * 小Q想要给他的朋友发送一个神秘字符串，但是他发现字符串的过于长了，
+     * 于是小Q发明了一种压缩算法对字符串中重复的部分进行了压缩，
+     * 对于字符串中连续的m个相同字符串S将会压缩为[m|S](m为一个整数且1<=m<=100)，
+     * 例如字符串ABCABCABC将会被压缩为[3|ABC]，现在小Q的同学收到了小Q发送过来的字符串，你能帮助他进行解压缩么？
+     */
+    private static String replace(String str) {
+        if (str.contains("]")) {
+            int i =0;
+            while (i<=str.length()){
+                if ("]".charAt(0) == str.charAt(i++)){
+                    break;
+                }
+            }
+            int j = i-1;
+            while (0 <= j) {
+                char c = str.charAt(j--);
+                if ("[".charAt(0) == c) {
+                    String repeatStr = str.substring(j + 2, i - 1);
+                    String[] split = repeatStr.split("\\|");
+                    Integer num = Integer.parseInt(split[0]);
+                    String compress = split[1];
+                    StringBuilder builder = new StringBuilder();
+                    while (0 < num) {
+                        builder.append(compress);
+                        num--;
+                    }
+                    str = replace(str.replace("["+repeatStr+"]",builder.toString()));
+                }
+            }
+        }
+        return str;
+    }
+
     public static void main(String[] args) {
         practice practice = new practice();
         int[][] arr = {{0, 1}};
